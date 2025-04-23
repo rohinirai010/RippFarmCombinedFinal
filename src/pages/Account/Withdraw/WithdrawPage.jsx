05b0fc924645cf2bcbb5bbf5102e0d5490390a8a
rohinirai010 <rairohini2001@gmail.com>
2025-04-23 20:55:35 +0530

    chages done in ui

--- src/pages/Account/Withdraw/WithdrawPage.jsx
+++ src/pages/Account/Withdraw/WithdrawPage.jsx
@@ -181,7 +181,7 @@ const WithdrawalPage = () => {
           </div>
         </div>
       </motion.header>
-      <div className="flex flex-col items-center text-white px-3 sm:px-6 pt-8 sm:pb-8">
+      <div className="flex flex-col items-center text-white px-3 sm:px-6 pt-8 sm:pb-8 h-full">
         {/* Address Addition Box */}
         <div className="w-full bg-[#1e2938]/50 border border-[#3f89e2] rounded-2xl p-3 sm:p-6 mb-6">
           <div className="flex items-center justify-between mb-3">
